b0a437e9d32c1b18eae6d7890a80418e5452942e
martinDP25 <43508627+martinDP25@users.noreply.github.com>
2018-11-18 12:12:46 -0500

    Add files via upload

---
+++ LetsPlay.java
@@ -0,0 +1,11 @@
+
+public class LetsPlay {
+
+	public static void main(String[] args)
+	{
+		Dice test = new Dice(0, 0);
+		test.rollDice();
+		System.out.println(test);
+		System.out.println(test.rollDice());
+	}
+}
