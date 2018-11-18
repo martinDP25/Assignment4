1c86d6be673ef465218cdd82d305119d9ebb522c
martinDP25 <43508627+martinDP25@users.noreply.github.com>
2018-11-18 12:11:15 -0500

    Add files via upload

---
+++ Dice.java
@@ -0,0 +1,42 @@
+import java.util.Random;
+
+public class Dice {
+	
+	//Declaring 2 Dice attributes
+	private int dice1;
+	private int dice2;
+	
+	//Declaring random method variable
+	Random rand = new Random();
+	
+	//Dice constructor, default set to 0 for each dice.
+	public Dice(int d1, int d2)
+	{
+		dice1=d1=0;
+		dice2=d2=0;
+	}
+	
+	//Get method for both Dice variables
+	public int getDice1()
+	{
+		return dice1;
+	}
+	public int getDice2()
+	{
+		return dice2;
+	}
+	
+	//Method that makes use of random method to assign random numbers (1-6) to each die.
+	public int rollDice()
+	{
+		dice1=rand.nextInt(6) +1;
+		dice2=rand.nextInt(6) +1;
+		return dice1+dice2;
+	}
+	
+	//Method that returns a string stating each dice value
+	public String toString()
+	{
+		return("The value of the 1st dice is " + dice1 + " and the value of the 2nd dice is " + dice2);
+	}
+}
