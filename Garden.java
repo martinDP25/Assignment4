6af4c0795e1ca62888591d3c12d0447d286bcccb
martinDP25 <43508627+martinDP25@users.noreply.github.com>
2018-11-23 12:32:19 -0500

    Add files via upload
    Update: Done

--- Garden.java
+++ Garden.java
@@ -1,4 +1,3 @@
-
 public class Garden {
 
 	private char garden[][];
@@ -6,75 +5,122 @@ public class Garden {
 	//Constructor to set default garden size to 3x3.
 	public Garden()
 	{
-		garden[3][3]='-';
+		garden= new char[3][3];
 		initializeGarden();
 	}
 
 	//Constructor that takes an integer to set the garden size.
-	//If statement used to ensure garden size is 2x2 or greater. 
+	//If statement implemented to ensure garden size is 3x3 or greater. 
 	public Garden(int N)
 	{
 		if(N>=3)
 		{
-			char garden[][]=new char [N][N];
-		}else
+			garden=new char [N][N];
+		}else if(N<3)
+		{
 			System.out.println("Please pick a value equal to 3 or greater");
+			System.exit(0);
+		}
 		initializeGarden();
 	}
 	
-	public char initializeGarden()
+	private char initializeGarden()
 	{
-		/*for(int x = 0; x<garden.length; x++)
+		for(int x = 0; x<garden.length; x++)
 		{
 			for(int y=0; y<garden[x].length; y++)
 			{
 				garden[x][y]='-';
 			}
-			System.out.println("");
-		}*/
+		}
 		return 0;
 	}
 	
-	public int getInLocation(int r, int c)
+	public char getInLocation(int r, int c)
 	{
 		return garden[r][c];
 	}
 	
 	public char plantFlower(int r, int c)
 	{
-		return garden[r][c]='f';
+		garden[r][c]='f';
+		
+		return garden[r][c];
 	}
 	
 	public char plantTree(int r, int c)
 	{
+		garden[r][c]='t';
 		garden[r+1][c]='t';
 		garden[r][c+1]='t';
 		garden[r+1][c+1]='t';
+		
 		return garden[r][c];
 	}
 	
 	public char removeFlower(int r, int c)
 	{
-		return garden[r][c]='-';
+		garden[r][c]='-';
+		return 0;
 	}
 	
 	public int countPossibleTrees()
 	{
-		return 0;
+		int ST=0;
+		for(int x=0; x<garden.length-1; x++)
+		{
+			for(int y=0; y<garden[x].length-1; y++)
+			{
+				if(garden[x][y]=='-'&&garden[x+1][y]=='-'&&garden[x][y+1]=='-'&&garden[x+1][y+1]=='-')
+				{
+					ST+=1;
+				}
+			}
+		}
+		return ST;
 	}
 	
 	public int countPossibleFlowers()
 	{
-		return 0;
+		int SF=0;
+		for(int x=0; x<garden.length; x++)
+		{
+			for(int y=0; y<garden[x].length; y++)
+			{
+				if(garden[x][y]=='-')
+				{
+					SF+=1;
+				}
+			}
+		}
+		return SF;
 	}
 	
 	public boolean gardenFull()
 	{
+		for(int x=0; x<garden.length; x++)
+		{
+			for(int y=0; y<garden[x].length; y++)
+			{
+				if(garden[x][y]=='-')
+				{
+					return false;
+				}
+			}
+		}
 		return true;
 	}
 	
 	public String toString()
 	{
-		return ("The garden size is " + garden);
+		for(int x = 0; x<garden.length; x++)
+		{
+			for(int y=0; y<garden[x].length; y++)
+			{
+				System.out.print(garden[x][y] + " ");
+			}
+			System.out.println("");
+		}
+		return ("The garden size is " + garden.length + "x" + garden.length + "\n");
 	}
 }
